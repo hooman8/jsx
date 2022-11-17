511da464c14e75b125e93493eca5ac03b4e4c1d4
Hooman Pegahmehr <hpegahmehr@gmail.com>
2022-11-16 22:51:27 -0500

    extracting components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  //   const date = new Date();
+  //   const time = date.toLocaleTimeString();
+  //   return <input type="number" min={5} />;
+  return (
+    <textarea
+      readOnly
+      maxLength={3}
+      spellCheck
+      style={{ backgroundColor: "gray" }}
+    />
+  );
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 // Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 // Get reference to the div with ID root
 const el = document.getElementById("root");
 // Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
-// Create a component
-function App() {
-  //   const date = new Date();
-  //   const time = date.toLocaleTimeString();
-  //   return <input type="number" min={5} />;
-  return (
-    <textarea
-      readOnly
-      maxLength={3}
-      spellCheck
-      style={{ backgroundColor: "gray" }}
-    />
-  );
-}
+
 // Show the component on the screen
 root.render(<App />);
